162589748d61302298f536d1f53edc9af5ffdf7e
unknown <ekdud6887@gmail.com>
2023-12-15 16:01:22 +0900

    Studyed

---
+++ module_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 모듈 - 변수, 함수, 클래스가 모여있는 것
+# 
+# # 간단한 함수 만들어서 모듈화 시켜보자
+
+# In[4]:
+
+
+def addNum(num1,num2):
+    return num1+num2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
